94a68d739c504b8938ee43c7605b00a365c5fe14
ruslan <sonikx94@yandex.ru>
2025-10-30 13:41:59 +0300

    Add simple worker pool example.

--- main.go
+++ main.go
@@ -1,9 +1,23 @@
 package main
 
 import (
-	"fmt"
+	"time"
+
+	"github.com/1PALADIN1/worker-pool/worker_pool"
+)
+
+const (
+	numberOfWorkers = 20
+	tasksAmount     = 2_000
 )
 
 func main() {
-	fmt.Println("Hello world!")
+	wp := worker_pool.NewWorkerPool(numberOfWorkers)
+	for range tasksAmount {
+		wp.Submit(func() {
+			time.Sleep(200 * time.Millisecond)
+		})
+	}
+
+	wp.StopWait()
 }
